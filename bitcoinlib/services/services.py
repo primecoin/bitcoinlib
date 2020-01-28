9872d22e7295b89d278441147524aa5505dbff91
Sunny King <sunnyking9999@gmail.com>
2020-01-28 17:49:53 +0000

    Experimental settings for Primecoin network Experimental service provider for primecoind

--- bitcoinlib/services/services.py
+++ bitcoinlib/services/services.py
@@ -127,7 +127,7 @@ class Service(object):
             if self.resultcount >= self.max_providers:
                 break
             try:
-                if sp not in ['bitcoind', 'litecoind', 'dashd'] and not self.providers[sp]['url'] and \
+                if sp not in ['primecoind', 'bitcoind', 'litecoind', 'dashd'] and not self.providers[sp]['url'] and \
                         self.network.name != 'bitcoinlib_test':
                     continue
                 client = getattr(services, self.providers[sp]['provider'])
